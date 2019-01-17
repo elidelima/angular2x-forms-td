9ee7487c4bd334b8f2f7a2bffb0cca7f1c7eedeb
elidelima <eli.lima@alpar.com.br>
2019-01-17 11:59:52 -0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log("FML")
+  }
+}
