58b4712bd60edda276ae029f4fa206526ecc4537
elidelima <eli.lima@alpar.com.br>
2019-02-06 14:34:01 -0200

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,51 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') ngForm : NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.ngForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+    this.ngForm.form.patchValue({
+      userData : { 
+        username : suggestedName
+      }
+    })
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
-    console.log("FML")
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   console.log("FML")
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.ngForm);
+    this.user.username = this.ngForm.value.userData.username;
+    this.user.email = this.ngForm.value.userData.email;
+    this.user.secretQuestion = this.ngForm.value.secret;
+    this.user.answer = this.ngForm.value.questionAnswer;
+    this.user.gender = this.ngForm.value.gender;
+
+    this.ngForm.reset();
   }
 }
